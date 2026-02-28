3d62e3e22f7a3d3fda99ca45e2c493f688f505d1
baarayy <albaraa.ah26@icloud.com>
2026-02-28 15:50:14 +0200

    fix: CORs issues

--- redirect-service/src/main.ts
+++ redirect-service/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: "*" });
   const port = process.env.PORT || 8082;
   await app.listen(port);
   console.log(`Redirect Service running on port ${port}`);
